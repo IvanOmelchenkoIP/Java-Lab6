f0d2e3ccd46000961eaab23358baac10d297c56a
IvanOmelchenkoIP <io25112002@gmail.com>
2022-11-20 21:06:46 +0200

    Implement scanner
    Implement InputScanner class

--- java/src/lab6/scanner/InputScanner.java
+++ java/src/lab6/scanner/InputScanner.java
@@ -1,5 +1,20 @@
 package lab6.scanner;
 
+import java.util.Scanner;
+
 public class InputScanner {
 
+	private Scanner scanner;
+
+	public InputScanner() {
+		scanner = new Scanner(System.in);
+	}
+
+	public String scanInput() {
+		return scanner.nextLine();
+	}
+
+	public void close() {
+		scanner.close();
+	}
 }
