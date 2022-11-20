6528ff025c50a43f7f43cd5f8bb07531efd4b7e1
IvanOmelchenkoIP <io25112002@gmail.com>
2022-11-20 22:02:29 +0200

    Implement MenuModel
    Implement MenuModel class so that it calls add and translate methods of
    dictionary

--- java/src/lab6/menu/MenuModel.java
+++ java/src/lab6/menu/MenuModel.java
@@ -1,6 +1,15 @@
 package lab6.menu;
 
+import java.io.IOException;
+
 import lab6.dictionary.TranslateDictionary;
+
+class MenuOptions {
+
+	public final static String MENU_ADD_PARE = "1";
+	public final static String MENU_TRANSLATE = "2";
+	public final static String MENU_EXIT = "3";
+}
 
 public class MenuModel {
 
@@ -8,5 +17,32 @@ public class MenuModel {
 	
 	public void setControllers(TranslateDictionary dictionary) {
 		this.dictionary = dictionary;
+	}
+	
+	public int processCommand(String command) {
+		int status;
+		switch (command) {
+		case MenuOptions.MENU_ADD_PARE -> status = MenuStates.STATUS_ADD_PARE;
+		case MenuOptions.MENU_TRANSLATE -> status = MenuStates.STATUS_TRANSLATE;
+		case MenuOptions.MENU_EXIT -> status = MenuStates.STATUS_EXIT;
+		default -> status = MenuStates.STATUS_INCORRECT_COMMAND;
+		}
+		return status;
+	}
+
+	public void tagsByAlphabet(String en, String ua) throws IOException {
+		try {
+			dictionary.add(en, ua); 
+		} catch (IOException exception) {
+			throw exception;
+		}
+	}
+
+	public void tagsByOccurence(String phrase) throws IOException {
+		try {
+			dictionary.translate(phrase);
+		} catch (IOException exception) {
+			throw exception;
+		}
 	}
 }
