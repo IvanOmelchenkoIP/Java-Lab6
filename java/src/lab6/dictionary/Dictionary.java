02bb24b11dc883c1198a9970928675e95e9099d0
IvanOmelchenkoIP <io25112002@gmail.com>
2022-11-20 21:40:12 +0200

    Implement dictionary
    Implement dictionary class so that data can be added and translated

--- java/src/lab6/dictionary/Dictionary.java
+++ java/src/lab6/dictionary/Dictionary.java
@@ -1,5 +1,45 @@
 package lab6.dictionary;
 
+import java.util.HashMap;
+
 public class Dictionary {
 
+	HashMap<String, String> dictionary;
+
+	public Dictionary() {
+		dictionary = new HashMap<String, String>();
+	}
+
+	public void add(String en, String ua) {
+		if (dictionary.containsKey(en)) {
+			dictionary.replace(en, ua);
+		} else {
+			dictionary.put(en, ua);
+		}
+	}
+
+	public String translate(String phrase) {
+		String translation = "";
+		String[] words = phrase.split("\\s+");
+		for (String word : words) {
+			String translated = "";
+			if (word.matches("[+-]?\\d+(.\\d+)?") || word.matches("\\W")) {
+				translated = word;
+			} else {
+				String first = Character.toString(word.charAt(0));
+				String last = Character.toString(word.charAt(word.length() - 1));
+				if (first.matches("\\W")) {
+					word = word.substring(1);
+					translated += first;
+				}
+				if (last.matches("\\W")) {
+					word = word.substring(0, word.length() - 1);
+					translated += " " + last;
+				}
+				translated.replace(" ", dictionary.get(word));
+			}
+			translation += translated + " ";
+		}
+		return translation.substring(0, translation.length() - 1);
+	}
 }
