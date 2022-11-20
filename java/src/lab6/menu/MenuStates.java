054b2c7df23f207516b2abb7109e01725372daa4
IvanOmelchenkoIP <io25112002@gmail.com>
2022-11-20 21:57:51 +0200

    Implement menu states and view
    Implement menu states and view for future use

--- java/src/lab6/menu/MenuStates.java
+++ java/src/lab6/menu/MenuStates.java
@@ -1,5 +1,10 @@
 package lab6.menu;
 
-public class MenuStates {
+class MenuStates {
 
+	public static final int STATUS_INCORRECT_COMMAND = -1;
+	public static final int STATUS_EXIT = 0;
+	public static final int STATUS_ADD_PARE = 1;
+	public static final int STATUS_TRANSLATE = 2;
 }
+
--- java/src/lab6/menu/MenuView.java
+++ java/src/lab6/menu/MenuView.java
@@ -1,5 +1,32 @@
 package lab6.menu;
 
 public class MenuView {
+	public final static String MENU = "1) add pare of words\n" + "2) translate phrase\n" + "3) exit\n";
+	public final static String MENU_INPUT_FIELD = "Enter a corresponding number to continue: ";
+	
+	public final static String EN_INPUT = "Input a single word in English: ";
+	public final static String UA_INPUT = "Enter a single word in Ukrainian: ";
+	public final static String ADD_SUCCESS = "A pare of words successfully added...\n\n";
 
+	public final static String PHRASE_INPUT = "Enter a phrase to translate: ";
+	public final static String TRANSLATE_SUCCESS = "Translation of a phrase:";
+	public final static String TRANSLATE_ERROR = "Translation not successful:";
+
+	public final static String COMMAND_ERROR = "Error! Invalid command!\n\n";
+
+	public final static String EXIT = "Exiting the program...\n\n";
+
+	public void showMessage(String msg) {
+		System.out.print(msg);
+	}
+	
+	public void showTranslation(String msg, String translation) {
+		System.out.println(msg);
+		System.out.println(translation + "\n");
+	}
+
+	public void showException(String msg, Exception exception) {
+		System.out.println(msg);
+		System.out.println(exception.getMessage() + "\n");
+	}
 }
